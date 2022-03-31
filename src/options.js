7c5a9da1d717e2573ad39e201ff319dff4de75d1
Ihar Zharski <ihar.zharski@gmail.com>
2022-03-31 21:31:01 +0200

    make options to safe configuration to write notes to file

--- src/options.js
+++ src/options.js
@@ -1,18 +1,25 @@
+//form elements
+var txt_noteFormat = document.getElementById('note_format')
+var chb_addIntoFile = document.getElementById('add_into_file')
+var inpt_notesLocation = document.getElementById('notes_location')
+
 // Saves options to chrome.storage
 function save_options() {
-    var obsidianNoteFormat = document.getElementById('obsidian_note_format').value;
-    var selectionIntoDescription = document.getElementById('add_selection_into_description').checked;
+    var noteFormat = txt_noteFormat.value;
+    var addIntoFile = chb_addIntoFile.checked;
+    var notesLocation = inpt_notesLocation.value;
 
     chrome.storage.sync.set({
-        obsidianNoteFormat, obsidianNoteFormat,
-        selectionIntoDescription: selectionIntoDescription
+        noteFormat: noteFormat,
+        addIntoFile: addIntoFile,
+        notesLocation: notesLocation
     }, function() {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
         status.textContent = 'Changes are saved.';
         setTimeout(function() {
             status.textContent = '';
-        }, 950);
+        }, 300);
     });
 }
 
@@ -20,13 +27,21 @@ function save_options() {
 // stored in chrome.storage.
 function load_from_storage() {
     chrome.storage.sync.get({
-        selectionIntoDescription: false,
-        obsidianNoteFormat: `[{title}]({url})`
+        noteFormat: `| [{title}]({url}) | |`,
+        addIntoFile: false,
+        notesLocation: ''
     }, function(options) {
-        document.getElementById('obsidian_note_format').value = options.obsidianNoteFormat;
-        document.getElementById('add_selection_into_description').checked = options.selectionIntoDescription;
+        txt_noteFormat.value = options.noteFormat;
+        chb_addIntoFile.checked = options.addIntoFile;
+        inpt_notesLocation.disabled = !options.addIntoFile;
+        inpt_notesLocation.value = options.notesLocation
     });
+}
+
+function toogle_folder_input(){
+    inpt_notesLocation.disabled = !chb_addIntoFile.checked
 }
 
 document.addEventListener('DOMContentLoaded', load_from_storage);
 document.getElementById('save').addEventListener('click', save_options);
+chb_addIntoFile.addEventListener('click', toogle_folder_input);
